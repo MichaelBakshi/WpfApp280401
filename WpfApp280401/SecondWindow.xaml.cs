8a315296804f00de3f1e201d4a15665aeab1cbfe
Michael Bakshi <michael.bakshi83@gmail.com>
2021-04-29 09:51:13 +0300

    k

--- WpfApp280401/SecondWindow.xaml.cs
+++ WpfApp280401/SecondWindow.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp280401
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SecondWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SecondWindow : Window
     {
-        public MainWindow()
+        public SecondWindow()
         {
             InitializeComponent();
-        }
-
-        private void Open_Window(object sender, RoutedEventArgs e)
-        {
-            SecondWindow objsecondwindow = new SecondWindow();
-            this.Visibility = Visibility.Hidden;
-            objsecondwindow.Show();
         }
     }
 }
